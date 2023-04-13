20caabd5cc325415be904cc80b40f2086f7d2bc9
vlada_caban <vladaonly@gmail.com>
2023-04-13 19:01:16 -0400

    updated js file with functions to check length and charset

--- script.js
+++ script.js
@@ -5,19 +5,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  // Asking user to input length and checking to be sure it is between 8 and 128
-  var passwordLength = Number(
-    window.prompt("Please enter password length between 8 and 128:", "8")
-  );
-  while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = Number( window.prompt(
-      "Please enter a valid length number between 8 and 128:",
-      "8")
-    );
-  }
+  var passwordLength = checkLength();
+  var allPossibleChar = checkCharSet();
 
-  //original code 
-  var password = generatePassword(passwordLength);
+  var password = generatePassword(passwordLength, allPossibleChar);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -27,58 +18,53 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//var passwordLength = 0; //user input length
+//ADD function to check password length
+function checkLength() {
+  var passwordLength = Number(
+    window.prompt("Please enter password length between 8 and 128:", "8")
+  );
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = Number(
+      window.prompt("Please enter a valid length number between 8 and 128:", "8")
+    );
+  }
 
-//need to add checkpoint to make sure passwordLength between 8 and 128
-
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var numbersChar = "0123456789";
-var specialChar = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
-
-var includeUppercase = false;
-var includeLowercase = false;
-var includeNumbers = false;
-var includeSpecialChar = false;
-
-//need to add checkpoint that at least one of the above conditions is TRUE / at least one box checked on the HTML form (IF okay to use form)
-
-//to test
-includeUppercase = true;
-includeLowercase = false;
-includeNumbers = true;
-includeSpecialChar = true;
-//passwordLength = 10;
-
-//need create a string with all possible characters based on user input
-var allPossibleChar = "";
-
-if (includeUppercase == true) {
-  allPossibleChar = uppercaseChar;
-}
-if (includeLowercase == true) {
-  allPossibleChar = allPossibleChar + lowercaseChar;
+  return passwordLength;
 }
 
-if (includeNumbers == true) {
-  allPossibleChar = allPossibleChar + numbersChar;
+//ADD function to check what characters 
+function checkCharSet() {
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var numbersChar = "0123456789";
+  var specialChar = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
+
+  includeUppercase = true;
+  includeLowercase = false;
+  includeNumbers = true;
+  includeSpecialChar = true;
+
+  var allPossibleChar = "";
+
+  if (includeUppercase == true) {
+    allPossibleChar = uppercaseChar;
+  }
+  if (includeLowercase == true) {
+    allPossibleChar = allPossibleChar + lowercaseChar;
+  }
+  if (includeNumbers == true) {
+    allPossibleChar = allPossibleChar + numbersChar;
+  }
+  if (includeSpecialChar == true) {
+    allPossibleChar = allPossibleChar + specialChar;
+  }
+
+return allPossibleChar;
+
 }
 
-if (includeSpecialChar == true) {
-  allPossibleChar = allPossibleChar + specialChar;
-}
-
-//to test
-console.log(allPossibleChar);
-
-var lengthOfAvailChar = allPossibleChar.length;
-
-//to test
-console.log(lengthOfAvailChar);
-
-//var generatedPassword = ""; 
-
-function generatePassword(passwordLength) {
+function generatePassword(passwordLength, allPossibleChar) {
+  var lengthOfAvailChar = allPossibleChar.length;
   var generatedPassword = ""; //string where new password will be added to
   var i = 0;
   while (i < passwordLength) {
@@ -90,7 +76,3 @@ function generatePassword(passwordLength) {
   return generatedPassword;
 }
 
-//generatedPassword = generatePassword(passwordLength);
-
-// to test
-//console.log(generatedPassword);
